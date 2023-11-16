baa0710c1cf4c8dc6791c6d221dee36bb63489de
pedrohenrique7799 <phsn.sgorlon@gmail.com>
2023-11-16 14:18:10 -0400

    funcao de produtos por id e js no html

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id:1, nome:"nome do produto", preco:"000,00", descricao:"texto qualquer", imagem:"/imagens/imagem.jpg"},
+  {id:2, nome:"nome do produto", preco:"000,00", descricao:"texto qualquer", imagem:"/imagens/imagem.jpg"},
+  {id:3, nome:"nome do produto", preco:"000,00", descricao:"texto qualquer", imagem:"/imagens/imagem.jpg"}
+]
+
+function buscarProdutoPorId(id){
+  const produto = produtos.find(produto => produto.id == id);
+  return produto || null
+}
+console.log(buscarProdutoPorId(3))
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'Olá, Mundo!' });
 });
